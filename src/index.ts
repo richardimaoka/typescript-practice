e9a1a4dd9aa3b80ccd6cd79aabebae68c1ebb5cb
richardimaoka <richard.s.imaoka@gmail.com>
2025-08-10 17:23:22 +0900

    type strengthened

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ interface A {
   gridTemplateColumns: string;
 }
 
-function propertyToField(propertyName: string): string {
+function propertyToField(propertyName: string): keyof A {
   if (propertyName === "grid-template-columns") {
     return "gridTemplateColumns";
   } else {
@@ -12,6 +12,9 @@ function propertyToField(propertyName: string): string {
 
 function main() {
   const a: A = { gridTemplateColumns: "auto auto 1fr" };
+  const field = propertyToField("grid-template-columns");
+  const result = a[field];
+
   console.log(JSON.stringify(a));
 }
 
