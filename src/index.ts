b6a83fa5425370276b0eb5343451ff8d767fd94f
richardimaoka <richard.s.imaoka@gmail.com>
2025-08-10 17:21:19 +0900

    pretty print

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, TypeScript!");
+interface A {
+  gridTemplateColumns: string;
+}
+
+function propertyToField(propertyName: string): string {
+  if (propertyName === "grid-template-columns") {
+    return "gridTemplateColumns";
+  } else {
+    throw new Error(`propertyName: "${propertyName}" is invalid`);
+  }
+}
+
+function main() {
+  const a: A = { gridTemplateColumns: "auto auto 1fr" };
+  console.log(JSON.stringify(a));
+}
+
+main();
